ce16f8d93eb92cf8f6f9721ea22af2abd9575c22
Ricardo Gonzalez Villegas <rikarudogonzarezu@gmail.com>
2020-07-23 16:54:53 -0400

    Updates

---
+++ precache-manifest.6049c77e0217707ba5468b8b0e500742.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff8a8c32501ecef762bc0eea6838817",
+    "url": "/currency-tracker/index.html"
+  },
+  {
+    "revision": "32f60e136b343a1399ca",
+    "url": "/currency-tracker/static/css/main.426b93b1.chunk.css"
+  },
+  {
+    "revision": "53ce5be7d8c2316e55c2",
+    "url": "/currency-tracker/static/js/2.6212c85e.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/currency-tracker/static/js/2.6212c85e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32f60e136b343a1399ca",
+    "url": "/currency-tracker/static/js/main.14558ea0.chunk.js"
+  },
+  {
+    "revision": "8279c4098d05cad48269",
+    "url": "/currency-tracker/static/js/runtime-main.e90161a4.js"
+  }
+]);
